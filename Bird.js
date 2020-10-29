8c273033dfedae225d65c8da09266a2bc4dc1374
Mr. Maxz <70730373+MrMaxz@users.noreply.github.com>
2020-10-29 15:58:04 -0400

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,19 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+
+    this.trajectory =[];
+  }
+
+  display() {
+
+    super.display();
+
+    if(this.body.velocity.x > 10 && this.body.position.x > 200){
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+    }
+   
+
+  }
+}
---
+++ Pig.js
@@ -0,0 +1,24 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+
+    this.Visiblity = 255;
+  }
+
+ display(){
+   //console.log(this.body.speed);
+   if(this.body.speed < 3){
+    super.display();
+   }
+   else{
+     World.remove(world, this.body);
+     push();
+     this.Visiblity = this.Visiblity - 5;
+     pop();
+   }
+   
+ }
+
+
+
+};
